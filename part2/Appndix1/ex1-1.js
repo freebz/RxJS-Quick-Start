1dbdd3e3f5e4135080ea28814efc2d8b8ced0e90
FreeBz <freebz@hananet.net>
2019-10-31 11:26:49 +0900

    부록1. RxJS의 Subjects

---
+++ part2/Appndix1/ex1-1.js
@@ -0,0 +1,32 @@
+const { BehaviorSubject } = rxjs;
+const subject = new BehaviorSubject("start");
+subject.subscribe({
+    next: (v) => console.log(`observerA: ${v}`),
+    complete: () => console.log("observerA completed")
+});
+
+subject.next(1);
+subject.next(2);
+
+subject.subscribe({
+    next: (v) => console.log(`observerB: ${v}`),
+    complete: () => console.log("observerB completed")
+});
+
+subject.complete();
+
+setTimeout(() => {
+    subject.subscribe({
+	next: (v) => console.log(`observerC: ${v}`),
+	complete: () => console.log("observerC completed")
+    });
+}, 2000);
+
+// 결과
+// observerA: start
+// observerA: 1
+// observerA: 2
+// observerB: 2
+// observerA completed
+// observerB completed
+// observerC completed
